2d6799400dec9b428a718e323b32d3aee319ac2e
Chris Mountford <chris@atlassian.com>
2016-01-24 22:28:09 +1100

    cleanup

--- robot.py
+++ robot.py
@@ -9,14 +9,15 @@ PORT1 = 1
 PORT2 = 5
 PORT3 = 0
 
-
+# noinspection PyAttributeOutsideInit
 class MyRobot(wpilib.IterativeRobot):
+
     def robotInit(self):  # creates all the objects needed to operate the robot
         self.pdp = wpilib.PowerDistributionPanel()
-        self.motor1 = wpilib.VictorSP(MOTOR1_PWM)
-        self.motor2 = wpilib.VictorSP(MOTOR2_PWM)
-        self.motor3 = wpilib.VictorSP(MOTOR3_PWM)
-        self.motor4 = wpilib.VictorSP(MOTOR4_PWM)
+        self.leftFront = wpilib.VictorSP(MOTOR1_PWM)
+        self.leftBack = wpilib.VictorSP(MOTOR2_PWM)
+        self.rightFront = wpilib.VictorSP(MOTOR3_PWM)
+        self.rightBack = wpilib.VictorSP(MOTOR4_PWM)
         self.stick = wpilib.Joystick(PORT3)
         self.multiplier = 1  # creates a multiplayer to adjust the speed
         self.throttle_toggle = False
@@ -36,30 +37,30 @@ class MyRobot(wpilib.IterativeRobot):
         right_value = -forward - turn
         right_value *= math.fabs(right_value)
         right_value *= self.multiplier
-        self.motor1.set(left_value)
-        self.motor2.set(left_value)
-        self.motor3.set(-right_value)
-        self.motor4.set(-right_value)
+        self.leftFront.set(left_value)
+        self.leftBack.set(left_value)
+        self.rightFront.set(-right_value)
+        self.rightBack.set(-right_value)
 
-    def autonomousInit(self):                                       #resets and starts the timer at the beginning of autonomous
+    def autonomousInit(self):
         self.logger.info("Autonomous Mode")
+        # resets and starts the timer at the beginning of autonomous
         self.timer.reset()
         self.timer.start()
 
     # The following lines tell the robot what to do in autonomous
     def autonomousPeriodic(self):
-        if self.timer.get() > 2:
-            self.arcade_drive(0,
-                              0)  # this sets up code that makes the robot drive forward for two seconds, hence the timer
-        else:
+        # this sets up code that makes the robot drive forward for two seconds, hence the timer
+        if self.timer.get() < 2:
             self.arcade_drive(0.7, 0)  # sets the robot to drive forward at 0.7 of the normal speed
+        else:
+            self.arcade_drive(0, 0)
 
-        # The following lines tell the robot what to do in teleop
-
+    # The following lines tell the robot what to do in teleop
     def teleopInit(self):
         self.logger.info("Teleoperated Mode")
 
-# The following lines tell the robot what to do in teleop
+    # The following lines tell the robot what to do in teleop
     def teleopPeriodic(self):
         self.arcade_drive(-self.stick.getY(), -self.stick.getZ())  # Controls the Joystick, forwards, backwards,turn
         if self.stick.getRawButton(
@@ -77,16 +78,15 @@ class MyRobot(wpilib.IterativeRobot):
         if self.throttle_toggle:
             self.multiplier = (-self.stick.getThrottle() + 1) / 2
 
-        # These lines are needed to turn the motors off when the robot is disabled
-
+    # These lines are needed to keep the motors turned off when the robot is disabled
     def disabledPeriodic(self):
-        self.motor1.set(0)
-        self.motor2.set(0)
-        self.motor3.set(0)
-        self.motor4.set(0)
+        self.leftFront.set(0)
+        self.leftBack.set(0)
+        self.rightFront.set(0)
+        self.rightBack.set(0)
 
 # The following lines of code are ALWAYS needed to deploy code onto the robot
 if __name__ == '__main__':
     wpilib.run(MyRobot)
 
-# By Alex Rowell, Ben Briganti, Lucca Buonamano and Lex Martin
+# By Alex Rowell, Ben Briganti, Lucca Buonamano, Blake Mountford and Lex Martin
