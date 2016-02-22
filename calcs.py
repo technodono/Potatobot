e5dd077a228986d94624fb5b4942bd94df704dc3
Chris Mountford <chris@atlassian.com>
2016-02-22 20:56:21 +1100

    updated calcs to write the whole config.json file directly and to use the json library instead of hand-rolled string concat

--- calcs.py
+++ calcs.py
@@ -1,13 +1,11 @@
+import json
 
 # helper program for calculating coordinates of boulders on the field
+# TODO make this print the whole config.json file
 
 
 def inches_in_feet(inches):
     return inches / 12
-
-
-def pair(point):
-    return "[{0:.3f}, {1:.3f}]".format(point[0], point[1])
 
 ballSize = inches_in_feet(10.0)
 midLineX = 27
@@ -18,12 +16,13 @@ barrierSeparation = inches_in_feet(50)
 defense_sep = inches_in_feet(52.5)
 
 
-# make a boulder centered at x,y
+# make a boulder centered at x,y, returns array(8) of points which are arrays(2)
 def make_boulder(x, y, edge=defaultEdge):
     b = []
+    # will be an irregular "octagon", vertices at 1/3 and 2/3 on each side of a square
     # leftmost top point
-    x1 = x - edge / 6
-    y1 = y - edge / 2
+    x1 = x - edge / 2
+    y1 = y - edge * 2 / 3
     b.append([x1, y1])
     # rightmost top point
     x2 = x1 + edge
@@ -31,58 +30,117 @@ def make_boulder(x, y, edge=defaultEdge):
     b.append([x2, y2])
     # topmost right point
     x3 = x2 + edge
-    y3 = y2 + edge
+    y3 = y1 + edge
     b.append([x3, y3])
     # bottommost right point
     x4 = x3
     y4 = y3 + edge
     b.append([x4, y4])
     # rightmost bottom point
-    x5 = x4 - edge
+    x5 = x2
     y5 = y4 + edge
     b.append([x5, y5])
     # leftmost bottom point
-    x6 = x5 - edge
+    x6 = x1
     y6 = y5
     b.append([x6, y6])
     # bottommost left point
-    x7 = x6 - edge
-    y7 = y6 - edge
+    x7 = x1 - edge
+    y7 = y4
     b.append([x7, y7])
     # topmost left point
     x8 = x7
-    y8 = y7 - edge
+    y8 = y3
     b.append([x8, y8])
-    return b
+
+    return {
+        'color': "thistle3",
+        'label': format("boulder at %d,%d" % (x, y)),
+        'points': b
+    }
+
+# make an array of point tuples in draw-order for a rectangle origin x,y width w height h
+def rect(x, y, w, h):
+    return [[x, y], [x + w, y], [x + w, y + h], [x, y + h]]
 
 
-def print_boulder(n, b):
-    print(',{')
-    print('"color": "purple",')
-    print("\"label\": \"boulder %d\"," % n)
-    print('"points": [' + pair(b[0]), pair(b[1]), pair(b[2]), pair(b[3]), pair(b[4]), pair(b[5]), pair(b[6]), pair(b[7]) + "]", sep=', ')
-    print('}')
+def make_barrier(x, y, length=4, thick=inches_in_feet(1)):
+    return {
+        'color': "black",
+        'label': "defense divider",
+        'points': rect(x, y, length, thick)
+    }
 
 
-print_boulder(1, make_boulder(midLineX, boulderSeparation))
-print_boulder(2, make_boulder(midLineX, boulderSeparation * 2))
-print_boulder(3, make_boulder(midLineX, boulderSeparation * 3))
-print_boulder(4, make_boulder(midLineX, boulderSeparation * 4))
-print_boulder(5, make_boulder(midLineX, boulderSeparation * 5))
-print_boulder(6, make_boulder(midLineX, boulderSeparation * 6))
+config = {
+    'pyfrc': {
+        'robot': {
+            "w": 2.4,
+            "h": 2,
+            "starting_x": 25,
+            "starting_y": 20,
+            "starting_angle": 0
+        },
+        "field": {
+            "w": 54,
+            "h": 27,
+            "px_per_ft": 12,
+            "objects": [
+                {
+                    "color": "thistle3",
+                    "label": "blue alliance defenses",
+                    "points": [[19.95, 4.458], [15.95, 4.458], [15.95, 26.643], [19.95, 26.643]]
+                },
+                {
+                    "color": "light slate gray",
+                    "label": "platform",
+                    "points": [[16.95, 4.458], [18.95, 4.458], [18.95, 26.643], [16.95, 26.643]]
+                },
+                {
+                    "color": "thistle3",
+                    "label": "red alliance defenses",
+                    "points": [[34.05, 0], [38.08, 0], [38.08, 22.643], [34.05, 22.643]]
+                },
+                {
+                    "color": "light slate gray",
+                    "label": "platform",
+                    "points": [[35.05, 0], [37.08, 0], [37.08, 22.643], [35.05, 22.643]]
+                },
+                {
+                    "color": "red",
+                    "label": "red alliance secret passage",
+                    "points": [[30, 27], [30, 23], [54, 23], [54, 27]]
+                },
 
+                {
+                    "color": "blue",
+                    "label": "blue alliance secret passage",
+                    "points": [[0, 4], [0, 0], [24, 0], [24, 4]]
+                },
+                {
+                    "color": "black",
+                    "label": "centre line",
+                    "points": [[27, 0], [27.1, 0], [27.1, 27], [27, 27]]
+                }
+            ]
+        }
+    }
+}
 
-# make a barrier with top left corner at x,y and thickness of thick
-def print_barrier(x, y, length=4, thick=0.1):
-    print(',{')
-    print('"color": "black",')
-    print("\"label\": \"defense divider\",")
-    print('"points": [' + pair([x, y]), pair([x + length, y]), pair([x + length, y + thick]), pair([x, y + thick]) + ']', sep=', ')
-    print('}')
+objects = config['pyfrc']['field']['objects']
 
 for i in range(6):
-    print_barrier(15.95, 4.458 + i * defense_sep)
+    boulder = make_boulder(midLineX, boulderSeparation * (i + 1))
+    objects.append(boulder)
 
 for i in range(6):
-    print_barrier(34.05, 0 + i * defense_sep)
+    barrier = make_barrier(15.95, 4.458 + i * defense_sep)
+    objects.append(barrier)
+
+for i in range(6):
+    barrier = make_barrier(34.05, 0 + i * defense_sep)
+    objects.append(barrier)
+
+with open('sim/config.json', 'w') as f:
+    json.dump(config, f, indent=2, sort_keys=True)
 
