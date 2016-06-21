8352d10b254317b7ff970dc13882ca468af5b4cf
Chris Mountford <chris@think.net.au>
2016-06-21 14:48:45 +1000

    added check to use imp to try to find pygame, also some small refactoring

--- sanity.py
+++ sanity.py
@@ -3,11 +3,10 @@
 import sys
 import platform
 from distutils.spawn import find_executable
-import subprocess
+from subprocess import PIPE, Popen
 import imp
 import re
 
-# platform.system() -> "Windows", "Darwin" or "Linux"
 MAGENTA = '\033[95m'
 BLUE = '\033[94m'
 GREEN = '\033[92m'
@@ -16,9 +15,6 @@ RED = '\033[91m'
 NORMAL = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
-
-CHECK='\u2714'
-CROSS='\u2716'
 
 def green(text):
     return GREEN + text + NORMAL
@@ -44,34 +40,50 @@ def fatal(result, name, instructions=""):
 def fatalexe(name): 
     fatal(find_executable(name), name)
 
-def format_version():
-    i = sys.version_info
+def format_version(i):
     return str(i.major) + "." + str(i.minor) + "." + str(i.micro)
 
 print(green(open('scripts/logo.txt').read()))
 
+# platform.system() -> "Windows", "Darwin" or "Linux"
 mac = platform.system() == "Darwin"
 windows = platform.system() == "Windows"
 linux = platform.system() == "Linux"
+pythonexe = "py" if windows else "python"
+i = sys.version_info
 
-if (sys.version_info.major < 3):
-    print ("This is " + magenta("Python " + str(sys.version_info.major)) + " but you need the latest 3.x")
+if (not windows):
+    CHECK='\u2714'
+    CROSS='\u2716'
+else:
+    # not sure why unicode escapes don't work on windows
+    CHECK='YES'
+    CROSS='NO'
+
+
+if (i.major < 3):
+    print ("This is " + magenta("Python " + str(i.major)) + " but you need the latest 3.x")
     if (not windows and find_executable("python3")):
         print(" you should start this script with " + magenta("python3"))
     elif (mac and find_executable("brew")): 
         print (" you're on " + bold("\uf8ff Mac") + " and have " + magenta("brew") + " so " + green("brew install python3"))
     exit(1)
 else:
-    print("checking " + magenta("python") + " version: " + format_version() + " " + green(CHECK))
+    print("checking " + magenta("python") + " version: " + format_version(i) + " " + green(CHECK))
 
 fatalexe("git")
 
-if (mac): 
-    if (find_executable("brew")):
-        p = subprocess.Popen(["brew", "list", "pygame"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+if (mac and find_executable("brew")):
+        cmd = ["brew", "list", "pygame"]
+        p = Popen(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True)
         (stdout, stderr) = p.communicate()
         p.wait()
         fatal(p.returncode == 0, "pygame", "brew install pygame --with-python3")
+
+try:
+    imp.find_module("pygame")
+except ImportError:
+    print(red(CROSS) + " Failed to find pygame module using imp")
             
 # TODO check if pygame is installed from windows
 
@@ -83,10 +95,13 @@ for module in modules:
         imp.find_module(module)
         print("checking python module " + magenta(module) + " " + green(CHECK))
     except ImportError:
-        fatal(False, module, "pip3 install " + module)
+        if (find_executable("pip3")):
+            fatal(False, module, "pip3 install " + module)
+        else:
+            fatal(False, module, pythonexe + " -m install " + module)
 
 # check git remote just in case somebody has the old one
-p = subprocess.Popen(['git', 'config', '--get', 'remote.origin.url'], stderr=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+p = Popen(['git', 'config', '--get', 'remote.origin.url'], stderr=PIPE, stdout=PIPE, universal_newlines=True)
 p.wait()
 if (p.returncode == 0):
     stdout, stderr = p.communicate()
